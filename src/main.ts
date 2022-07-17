b884d94fccffb7e5a246eac988f3b3bec1cd71fe
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2022-07-17 09:43:55 +0300

    feat: open yaml

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule } from '@nestjs/swagger';
+import { dirname, join } from 'path';
+import { parse } from 'yaml';
 import 'dotenv/config';
+import { readFile } from 'fs/promises';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +15,13 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+  const docs = await readFile(
+    join(dirname(__dirname), 'doc', 'api.yaml'),
+    'utf-8',
+  );
+  const parse_yaml = parse(docs);
+
+  SwaggerModule.setup('doc', app, parse_yaml);
   await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
