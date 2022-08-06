b97c86d20bc2093ef47f927dfbd352e1d537b7b3
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2022-08-06 14:56:42 -0400

    feat: change app

--- src/app.module.ts
+++ src/app.module.ts
@@ -27,6 +27,6 @@ import {ConfigModule} from "@nestjs/config";
       ConfigModule.forRoot({isGlobal: true})
   ],
   controllers: [AppController],
-  providers: [AppService, { provide: APP_GUARD, useClass: AtGuard }],
+  providers: [AppService],
 })
 export class AppModule {}
