e308f859ba75284f9da631d021e5ea0162d3bc80
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2022-08-07 11:49:24 -0400

    feat: simpify middeware

--- src/utils/logger/logger.middleware.ts
+++ src/utils/logger/logger.middleware.ts
@@ -1,29 +1,21 @@
 import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
-import {writeLog} from "../loggerRequest";
+import { writeLog } from '../loggerRequest';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
-    private readonly logger: Logger = new Logger(LoggerMiddleware.name);
+  private readonly logger: Logger = new Logger(LoggerMiddleware.name);
 
-    use(req: Request, res: Response, next: NextFunction) {
-        function RequestInfo(url, queryParameters, body, statusCode) {
-            this.url = url;
-            this.query_parameters = queryParameters;
-            this.body = body;
-            this.statusCode = statusCode;
-        }
-
-        const requestInfo = new RequestInfo(
-            req.url,
-            req.query,
-            req.body,
-            res.statusCode,
-        );
-
-        console.table(requestInfo);
-        //writeLog(requestInfo);
-        this.logger.log(requestInfo);
-        next();
-    }
+  use(req: Request, res: Response, next: NextFunction) {
+    console.log('New log: ');
+    const requestInfo = {
+      url: req.url,
+      query_parameters: req.query,
+      body: req.body,
+      statusCode: res.statusCode,
+    };
+    //writeLog(requestInfo);
+    this.logger.log(requestInfo);
+    next();
+  }
 }
